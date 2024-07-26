a02195cfa12948e969bb9dc9cf01cdbe79331ab4
guang <quark_gluon_plasma@163.com>
2024-07-26 22:45:27 +0800

    feat: react playground babel compile + iframe preview

---
+++ react-playground-project/src/ReactPlayground/components/Preview/compiler.ts
@@ -0,0 +1,100 @@
+import { transform } from '@babel/standalone'
+import { File, Files } from '../../PlaygroundContext'
+import { ENTRY_FILE_NAME } from '../../files'
+import { PluginObj } from '@babel/core';
+
+export const beforeTransformCode = (filename: string, code: string) => {
+    let _code = code
+    const regexReact = /import\s+React/g
+    if ((filename.endsWith('.jsx') || filename.endsWith('.tsx')) && !regexReact.test(code)) {
+      _code = `import React from 'react';\n${code}`
+    }
+    return _code
+}
+
+export const babelTransform = (filename: string, code: string, files: Files) => {
+    let _code = beforeTransformCode(filename, code);
+    let result = ''
+    try {
+        result = transform(_code, {
+        presets: ['react', 'typescript'],
+        filename,
+        plugins: [customResolver(files)],
+        retainLines: true
+        }).code!
+    } catch (e) {
+        console.error('编译出错', e);
+    }
+    return result
+}
+
+const getModuleFile = (files: Files, modulePath: string) => {
+    let moduleName = modulePath.split('./').pop() || ''
+    if (!moduleName.includes('.')) {
+        const realModuleName = Object.keys(files).filter(key => {
+            return key.endsWith('.ts') 
+                || key.endsWith('.tsx') 
+                || key.endsWith('.js')
+                || key.endsWith('.jsx')
+        }).find((key) => {
+            return key.split('.').includes(moduleName)
+        })
+        if (realModuleName) {
+            moduleName = realModuleName
+        }
+      }
+    return files[moduleName]
+}
+
+const json2Js = (file: File) => {
+    const js = `export default ${file.value}`
+    return URL.createObjectURL(new Blob([js], { type: 'application/javascript' }))
+}
+
+const css2Js = (file: File) => {
+    const randomId = new Date().getTime()
+    const js = `
+(() => {
+    const stylesheet = document.createElement('style')
+    stylesheet.setAttribute('id', 'style_${randomId}_${file.name}')
+    document.head.appendChild(stylesheet)
+
+    const styles = document.createTextNode(\`${file.value}\`)
+    stylesheet.innerHTML = ''
+    stylesheet.appendChild(styles)
+})()
+    `
+    return URL.createObjectURL(new Blob([js], { type: 'application/javascript' }))
+}
+
+function customResolver(files: Files): PluginObj {
+    return {
+        visitor: {
+            ImportDeclaration(path) {
+                const modulePath = path.node.source.value
+                if(modulePath.startsWith('.')) {
+                    const file = getModuleFile(files, modulePath)
+                    if(!file) 
+                        return
+
+                    if (file.name.endsWith('.css')) {
+                        path.node.source.value = css2Js(file)
+                    } else if (file.name.endsWith('.json')) {
+                        path.node.source.value = json2Js(file)
+                    } else {
+                        path.node.source.value = URL.createObjectURL(
+                            new Blob([babelTransform(file.name, file.value, files)], {
+                                type: 'application/javascript',
+                            })
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+export const compile = (files: Files) => {
+  const main = files[ENTRY_FILE_NAME]
+  return babelTransform(ENTRY_FILE_NAME, main.value, files)
+}
--- react-playground-project/src/ReactPlayground/components/Preview/index.tsx
+++ react-playground-project/src/ReactPlayground/components/Preview/index.tsx
@@ -1,3 +1,54 @@
+import { useContext, useEffect, useState } from "react"
+import { PlaygroundContext } from "../../PlaygroundContext"
+import Editor from "../CodeEditor/Editor";
+import { compile } from "./compiler";
+import iframeRaw from './iframe.html?raw'
+import { IMPORT_MAP_FILE_NAME } from "../../files";
+
 export default function Preview() {
-    return <div>Preview</div>
+
+    const { files} = useContext(PlaygroundContext)
+    const [compiledCode, setCompiledCode] = useState('')
+
+    useEffect(() => {
+        const res = compile(files);
+        setCompiledCode(res);
+    }, [files]);
+
+    const getIframeUrl = () => {
+        console.log(files);
+        const res = iframeRaw.replace(
+            '<script type="importmap"></script>', 
+            `<script type="importmap">${
+                files[IMPORT_MAP_FILE_NAME].value
+            }</script>`
+        ).replace(
+            '<script type="module" id="appSrc"></script>',
+            `<script type="module" id="appSrc">${compiledCode}</script>`,
+        )
+        return URL.createObjectURL(new Blob([res], { type: 'text/html' }))
+    }
+
+    useEffect(() => {
+        setIframeUrl(getIframeUrl())
+    }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode]);
+
+    const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+
+    return <div style={{height: '100%'}}>
+        <iframe
+            src={iframeUrl}
+            style={{
+                width: '100%',
+                height: '100%',
+                padding: 0,
+                border: 'none',
+            }}
+        />
+        {/* <Editor file={{
+            name: 'dist.js',
+            value: compiledCode,
+            language: 'javascript'
+        }}/> */}
+    </div>
 }
